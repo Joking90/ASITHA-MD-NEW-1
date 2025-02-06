cc63e79085afe2c932b28ef2a5d9f48cc6cee8cc
Joking90 <117741062+Joking90@users.noreply.github.com>
2025-02-06 21:28:36 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=PuQHzaAA#M0rFGwwU0HQCd-VxvUBhVRQrOL8V_lWb11YIwRa6e0A' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+SUDO: process.env.SUDO === undefined ? '923339655127,923339655127' : process.env.SUDO,
+SESSION_NAME: process.env.PORT === undefined ? "abd afghan" : process.env.SESSION_NAME,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
